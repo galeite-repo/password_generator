2a88b10b59c21e322ecf9b7cd59b68cddfa5ece5
Gabriel Araujo Leite <galeite@ibm.com>
2022-04-20 19:38:32 -0300

    toast

--- script.js
+++ script.js
@@ -30,6 +30,42 @@ function generatePassword(){
 
 }
 function copyPassword (){
-    alert("Senha copiada com sucesso!")
     navigator.clipboard.writeText(novaSenha);
+    toast('Senha Copiada', 2000)
 }
+
+function fillWidth(elem, timer, limit) {
+	if (!timer) { timer = 3000; }	
+	if (!limit) { limit = 100; }
+	var width = 1;
+	var id = setInterval(frame, timer / 100);
+		function frame() {
+		if (width >= limit) {
+			clearInterval(id);
+		} else {
+			width++;
+			elem.style.width = width + '%';
+		}
+	}
+};
+
+function toast(msg, timer) {
+	if (!timer) { timer = 3000; }
+	var $elem = $("<div class='toastWrap'><span class='toast'>" + msg + "<b></b><div class='timerWrap'><div class='timer'></div></div></span></div>");
+	$("#toast").append($elem); //top = prepend, bottom = append
+	$elem.slideToggle(100, function() {
+		$('.timerWrap', this).first().outerWidth($elem.find('.toast').first().outerWidth() - 10);
+		if (!isNaN(timer)) {
+			fillWidth($elem.find('.timer').first()[0], timer);
+			setTimeout(function() {
+				$elem.fadeOut(function() {
+					$(this).remove();
+				});
+			}, timer);			
+		}
+	});
+}
+
+$("#toast").on("click", "b", function() {
+	$(this).closest('.toastWrap').remove();
+})
